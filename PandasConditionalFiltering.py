186980b4ebd282ce05d1f0b43ccbd6c7b8a5bafa
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-07-14 07:54:34 +0530

    Pandas conditional filtering

--- PandasConditionalFiltering.py
+++ PandasConditionalFiltering.py
@@ -13,3 +13,9 @@ print("Original DataFrame:\n", df)
 
 # Filter rows where Age is greater than 25
 print("\nPeople older than 25:\n", df[df['Age'] > 25])
+
+# Filter rows where Score is lesser than 80
+print("\nPeople with Score less than 80:\n", df[df['Score'] < 80])
+
+# Combine conditions using &
+print("\nPeople older than 25 with Score greater than 80:\n", df[(df['Age'] > 25) & (df['Score'] > 80)])

cd30972d2b7ca240eafcfed4fd3799538534e70c
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-08-06 14:21:35 +0530

    Adding new date

--- PandasDate.py
+++ PandasDate.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
 
 dates = pd.date_range(start="2023-01-01",periods=5,freq='D')
 print(dates)
+
+data = np.random.randint(100, size=5)
+df = pd.DataFrame({'Date': dates, 'Value': data})
+print(df)

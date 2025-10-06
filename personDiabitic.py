6baa9b8fa65901a72cfe43ec2899182ac48304af
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-10-06 09:28:37 +0530

    Predict diabetec enhanced

--- personDiabitic.py
+++ personDiabitic.py
@@ -1,7 +1,9 @@
+from matplotlib.pyplot import plt
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the diabetes dataset
 data = load_diabetes()
@@ -15,18 +17,28 @@ print(b)
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Feature scaling (helps regressin perform better)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
 # Create model
 model = LinearRegression()
 
 # Train the model
-model.fit(X_train, y_train)
+model.fit(X_train_scaled, y_train)
 
 # Make predictions
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 
 # Evaluate
 mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-print(f"Mean Squared Error: {mse}")
+print(f"Mean Squared Error: {round(mse, 2)}")
+print(f"R^2 Score: {round(r2, 2)}")
 print(f"Model Coefficients: {model.coef_}")
 print(f"Model Intercept: {model.intercept_}")
+
+

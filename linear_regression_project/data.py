8cd724ba96b0ba821f8985ee2470cc10a2689f46
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-10-12 21:31:19 +0530

    Evaluate code

--- linear_regression_project/data.py
+++ linear_regression_project/data.py
@@ -2,6 +2,19 @@ import numpy as np
 
 def generate_data():
     # Sample data
-    X = np.array([1, 2, 3, 4, 5])
-    y = np.array([3, 4, 2, 5, 6])
+    X = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+    y = np.array([3, 4, 2, 5, 6, 7, 8, 9])
     return X, y
+
+def train_test_split(X, y, test_size=0.25):
+    # Shuffle the data
+    indices = np.arange(X.shape[0])
+    np.random.shuffle(indices)
+    X, y = X[indices], y[indices]
+    
+    # Split the data
+    split_index = int(X.shape[0] * (1 - test_size))
+    X_train, X_test = X[:split_index], X[split_index:]
+    y_train, y_test = y[:split_index], y[split_index:]
+    
+    return X_train, X_test, y_train, y_test

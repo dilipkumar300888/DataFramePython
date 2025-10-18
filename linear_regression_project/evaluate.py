daa51c10fefba78c9fb145e9f3bc0e06e52867f8
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-10-18 08:40:35 +0530

    Evaluate code

--- linear_regression_project/evaluate.py
+++ linear_regression_project/evaluate.py
@@ -14,4 +14,5 @@ def root_mean_squared_error(y_true, y_pred):
 def r2_score(y_true, y_pred):
     ss_res= np.sum((y_true - y_pred) ** 2)
     ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
-    return 1 - (ss_res / ss_tot)
+    r2 = 1 - (ss_res / ss_tot)
+    return r2

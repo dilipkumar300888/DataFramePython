163c5aad28e73963131b6fca9c987211e1ac25e9
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-10-26 09:06:19 +0530

    Evaluate code

--- linear_regression_project/visualize.py
+++ linear_regression_project/visualize.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 def plot_results(X, y, y_pred_scratch, y_pred_sklearn):
+    print("\n Plotting the results...")
     plt.scatter(X, y, color='blue', label='Actual data')
     plt.plot(X, y_pred_scratch, color='red', label='Scratch Model')
     plt.plot(X, y_pred_sklearn, color='green', linestyle='--', label='Sklearn Model')

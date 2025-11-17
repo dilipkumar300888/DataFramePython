7c48bcd5f9bf098ddec8e3e17d77e5335e190aa1
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-11-17 20:46:36 +0530

    Created an api for external use

--- linear_regression_project/evaluate.py
+++ linear_regression_project/evaluate.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 def mean_absolute_error(y_true, y_pred):
+    print("mean_absolute_error")
     return np.mean(np.abs(y_true - y_pred))
 
 def mean_squared_error(y_true, y_pred):

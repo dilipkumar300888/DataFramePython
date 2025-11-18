c143c1262b2190e9ae12e61d96fb411312601595
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-11-18 18:31:21 +0530

    Created an api for external use

--- linear_regression_project/evaluate.py
+++ linear_regression_project/evaluate.py
@@ -13,6 +13,7 @@ def root_mean_squared_error(y_true, y_pred):
     return np.sqrt(mean_squared_error(y_true, y_pred))
 
 def r2_score(y_true, y_pred):
+    print("r2_score")
     ss_res= np.sum((y_true - y_pred) ** 2)
     ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
     r2 = 1 - (ss_res / ss_tot)

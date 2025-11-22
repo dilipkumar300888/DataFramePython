b28570706d2bbc6c8f485f5167c5f123a0386c1d
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-11-22 08:13:28 +0530

    Created an api for external use

--- linear_regression_project/linear_regression_multi.py
+++ linear_regression_project/linear_regression_multi.py
@@ -2,6 +2,7 @@ import numpy as np
 
 class LinearRegressionMulti:
     def __init__(self, learning_rate=0.01, n_iterations=1000):
+        print("\nInitializing LinearRegressionMulti model...\n")
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
         self.weights = None

68c2cdf98e03cbd8f48de085ce37280067ec0f2a
dilipkumar300888 <dilipkumar.300888@gmail.com>
2025-12-09 20:49:10 +0530

    Created an api for external use

--- linear_regression_project/visualize.py
+++ linear_regression_project/visualize.py
@@ -5,6 +5,7 @@ def plot_results(X, y, y_pred_scratch, y_pred_sklearn):
     plt.scatter(X, y, color='blue', label='Actual data')
     plt.plot(X, y_pred_scratch, color='red', label='Scratch Model')
     plt.plot(X, y_pred_sklearn, color='green', linestyle='--', label='Sklearn Model')
+    print("\n")
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Linear Regression: Scratch vs Sklearn')

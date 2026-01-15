00381ed5760ecb3dfb81c23060e10c87b1bb71ad
dilipkumar300888 <dilipkumar.300888@gmail.com>
2026-01-15 09:19:18 +0530

    Created an api for external use

---
+++ linear_regression_project/house_price_project/api/test_api.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "http://127.0.0.1:8000/predict"
+data = {
+    "area": 3500,
+    "bedrooms": 5,
+    "age": 6,
+    "distance": 6
+}
+
+response = requests.post(url, json=data)
+print(response.json())
